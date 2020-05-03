61333550e000380d4a9fce02c7a5a66a12fa0aef
unknown <caipaingenier@gmail.com>
2020-05-03 15:10:38 -0500

    añadiendo rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,46 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DevproductosComponent } from './components/devproductos/devproductos.component';
+import { DevcotizadorComponent } from './components/devcotizador/devcotizador.component';
+import { CatalogoComponent } from './components/catalogo/catalogo.component';
+import { ContactoComponent } from './components/contacto/contacto.component';
 
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'devproductos',
+  component: DevproductosComponent
+},
+
+  { path: 'devcotizador',
+  component: DevcotizadorComponent
+},
+
+  {path: 'catalogo',
+component: CatalogoComponent
+},
+
+{
+  path: 'contacto',
+  component: ContactoComponent
+},
+
+{path: '',
+pathMatch: 'full',
+redirectTo: 'catalogo'
+}
+
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+
+
+  imports: [
+       RouterModule.forRoot(routes,
+      {
+        enableTracing:true
+      }
+      )],
   exports: [RouterModule]
 })
 export class AppRoutingModule {
